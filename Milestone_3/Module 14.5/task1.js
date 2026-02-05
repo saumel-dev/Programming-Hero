4875e1e9d20703d1ad3e6574ef4c5cf6447a09f5
Saumel-Das <saumel.spr@gmail.com>
2026-02-05 12:47:23 +0600

    Porblem solved

---
+++ Milestone_3/Module 14.5/task1.js
@@ -0,0 +1,8 @@
+let fruits = ['guava', 'mango', 'lichi', 'orange', 'watermelon'];
+console.log(fruits[3]);
+fruits[2] = 'jambura';
+console.log(fruits);
+for(const fruit of fruits)
+{
+    console.log(fruit);
+}
