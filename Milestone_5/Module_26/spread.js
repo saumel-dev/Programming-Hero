7cea0d39971e09a62274033446f82edfa7ed1979
Saumel-Das <saumel.spr@gmail.com>
2026-02-26 20:36:13 +0600

    learned ... operator

--- Milestone_5/Module_26/spread.js
+++ Milestone_5/Module_26/spread.js
@@ -4,3 +4,25 @@ console.log((maxe));
 const numbers = [1, 2, 3, 4, 6, 9, 10];
 const max = Math.max(...numbers);
 console.log(max);
+
+const params = [45, 12, 3];
+function sum(x, y, z) {
+    return x + y + z;
+}
+const res = sum(...params); 
+console.log(res);
+
+// const arr1 = [1, 2, 3];
+// const arr2 = arr1;
+// arr2.push(4);
+// console.log(arr1, arr2);
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1, 55];
+// arr2.push(4);
+console.log(arr1, arr2);
+
+const person = {name: 'alice', age: 30};
+const person2 = {...person};
+person2.salary = 3600;
+console.log(person); console.log(person2);
+
