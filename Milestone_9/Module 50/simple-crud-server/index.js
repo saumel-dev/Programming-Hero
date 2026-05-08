65188651258419bb92ee3fcf15cec355fd204674
Saumel-Das <saumel.spr@gmail.com>
2026-05-08 23:10:45 +0600

    added a user route to get user data

--- Milestone_9/Module 50/simple-crud-server/index.js
+++ Milestone_9/Module 50/simple-crud-server/index.js
@@ -1,7 +1,50 @@
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://simpleCrudUser:Lp6lbhDMKS1lzq0e@cluster0.qtp9hqs.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+
+        const db = client.db("simple-crud");
+        const userCollection = db.collection("users");
+
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB");
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`Simple CRUD server running on port ${port}`);
 })
