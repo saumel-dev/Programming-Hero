c2b48ba59aac389bba623c35a6d019fdaf7666e8
Saumel-Das <saumel.spr@gmail.com>
2026-05-09 22:20:40 +0600

    user-management-server

--- Milestone_9/Module 50/simple-crud-server/index.js
+++ Milestone_9/Module 50/simple-crud-server/index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 
@@ -29,6 +29,49 @@ const run = async () => {
         app.get('/users', async (req, res) => {
             const cursor = userCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log('user id', id);
+            const query = {
+                // _id : id // won't work cause it's saved as object id
+                _id: new ObjectId(id)
+            }
+            const user = await userCollection.findOne(query);
+            res.send(user);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+        app.patch('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {
+                _id: new ObjectId(id)
+            }
+            const modifiedUser = req.body;
+            const updatedDocument = {
+                $set: {
+                    name: modifiedUser.name,
+                    email: modifiedUser.email,
+                    role: modifiedUser.role
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDocument);
             res.send(result);
         })
 
