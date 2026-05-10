55f02834ab17e67948666ad419029fc9690a7b31
Saumel-Das <saumel.spr@gmail.com>
2026-05-10 20:39:00 +0600

    server-site created successfully

--- Milestone_9/book-server/index.js
+++ Milestone_9/book-server/index.js
@@ -1,14 +1,11 @@
 const dns = require('node:dns');
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
-app.use(cors());
 app.use(express.json());
-
-const port = process.env.PORT || 8000;
+app.use(cors());
+const port = process.env.PORT || 8000
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://bookAppUser:wvMu642EDVhtdU2r@cluster0.qtp9hqs.mongodb.net/?appName=Cluster0";
@@ -22,30 +19,56 @@ const client = new MongoClient(uri, {
 });
 
 const run = async () => {
-    await client.connect();
-    const db = client.db('book-collection');
-    const bookCollection = db.collection('books');
+    try {
+        await client.connect();
+        const db = client.db('book-collection');
+        const bookCollection = db.collection('books');
 
-    app.get('/books', async (req, res) => {
-        const books = await bookCollection.find().toArray();
-        res.send(books);
-    })
+        app.get('/books', async (req, res) => {
+            const books = await bookCollection.find().toArray();
+            res.send(books);
+        })
 
-    app.get('/books/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const book = await bookCollection.findOne(query);
-        res.send(book);
-    })
+        app.get('/books/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const book = await bookCollection.findOne(query);
+            res.send(book);
+        })
 
-    app.post('/books', async (req, res) => {
-        const newBook = req.body;
-        const result = await bookCollection.insertOne(newBook);
-        res.send(result);
-    })
+        app.post('/books', async (req, res) => {
+            const newBook = req.body;
+            const result = await bookCollection.insertOne(newBook);
+            res.send(result);
+        })
+
+        app.delete('/books/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.patch('/books/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+
+            const updateBook = req.body;
+            const updateDoc = {
+                $set: updateBook
+            }
+            const result = await bookCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
 }
+
 run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 })
