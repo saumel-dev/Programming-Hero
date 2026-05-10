0b2d82c8bf372809dd518ca3083034fc3693492a
Saumel-Das <saumel.spr@gmail.com>
2026-05-10 14:11:49 +0600

    added get all books and partial book

--- Milestone_9/book-server/index.js
+++ Milestone_9/book-server/index.js
@@ -1,14 +1,16 @@
 const dns = require('node:dns');
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 8000
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 8000;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://bookAppUser:wvMu642EDVhtdU2r@cluster0.qtp9hqs.mongodb.net/?appName=Cluster0";
 
 const client = new MongoClient(uri, {
@@ -20,22 +22,30 @@ const client = new MongoClient(uri, {
 });
 
 const run = async () => {
-    try {
-        await client.connect();
-        const db = client.db('book-collection');
-        const bookCollection = client.collection('books');
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-    finally {
-        //  await client.close();
-    }
+    await client.connect();
+    const db = client.db('book-collection');
+    const bookCollection = db.collection('books');
+
+    app.get('/books', async (req, res) => {
+        const books = await bookCollection.find().toArray();
+        res.send(books);
+    })
+
+    app.get('/books/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const book = await bookCollection.findOne(query);
+        res.send(book);
+    })
+
+    app.post('/books', async (req, res) => {
+        const newBook = req.body;
+        const result = await bookCollection.insertOne(newBook);
+        res.send(result);
+    })
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
-
 app.listen(port, () => {
-    console.log(`Server is running on PORT ${port}`);
+    console.log(`server is running on port ${port}`);
 })
